7de6319b3df456271cac4d80f4f4a16ad33c845b
Michael Silva <michaelsss16@gmail.com>
2025-11-15 10:46:15 -0300

    feat: adiciona playback de nada tenho falta

--- src/Pages/Playbacks.js
+++ src/Pages/Playbacks.js
@@ -6,15 +6,34 @@ export const Playbacks = () =>
             <p>Os áudios a seguir devem ser utilizados para estudos das canções e aplicação das técnicas apresentadas durante o curso.</p>
             <p> Lembre-se de sempre realizar os exercícios de aquecimento vocal antes do estudo de repertório!</p>
 
-        <h3>A casa é sua - Casa Worship</h3>
+        <h2>A casa é sua - Casa Worship</h2>
         <audio controls>
 <source src="/michaelsilvacanto/Playbacks/ACasaESua.mp3" type="audio/wav" />
 Seu navegador não suporta o elemento de áudio.
 </audio>
 
-<h3> Deus me ama - Thales Roberto</h3>
+<h2> Deus me ama - Thales Roberto</h2>
 <audio controls>
 <source src="/michaelsilvacanto/Playbacks/DeusMeAma.mp3" type="audio/wav" />
+Seu navegador não suporta o elemento de áudio.
+</audio>
+
+<h2>De nada tenho falta - Jéssica Augusto</h2>
+<h3>Playback</h3>
+<audio controls>
+<source src="/michaelsilvacanto/Playbacks/De nada tenho falta - playback.mp3" type="audio/wav" />
+Seu navegador não suporta o elemento de áudio.
+</audio>
+
+<h3>Playback com voz</h3>
+<audio controls>
+<source src="/michaelsilvacanto/Playbacks/De nada tenho falta - vozes.mp3" type="audio/wav" />
+Seu navegador não suporta o elemento de áudio.
+</audio>
+
+<h3>Playback com guia</h3>
+<audio controls>
+<source src="/michaelsilvacanto/Playbacks/De nada tenho falta - guia.mp3" type="audio/wav" />
 Seu navegador não suporta o elemento de áudio.
 </audio>
 </div>
